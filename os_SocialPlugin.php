15c1c65f1a331122a6c2ffd4ad810e42310b2a83
Odd Egil H. Selnes <post@odde.org>
2012-04-07 19:56:56 +0200

    first version av shortcode added - something is buggy with the cache functionality

--- os_SocialPlugin.php
+++ os_SocialPlugin.php
@@ -1,6 +1,15 @@
 <?php
-/*
-* 	tjo bing!
-*/          
-echo "hello world!"; 
+/*       
+Plugin Name: Twitter sak
+Plugin URI: http://dev.odde.org
+Description: Simple shortcode
+Version: 1.0
+Author: Odd Egil Hegge Selnes
+Author URI: http://dev.odde.org
+*/
+
+// include the shortcode stuff //
+include 'os_twitterShortcode.php';
+     
+
 ?>
