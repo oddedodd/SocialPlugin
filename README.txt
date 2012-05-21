d2a792984c8705a8c6f95ae30ac50364f1cc88fb
Odd Egil H. Selnes <post@odde.org>
2012-05-21 19:56:32 +0200

    Added lightbox support

--- README.txt
+++ README.txt
@@ -5,11 +5,11 @@ By Odd Egil H. Selnes
 This plugin now supports:
 - Twitter and Facebook widgets
 - Instagram and Twitter Short codes
+- Instagram pictures are now displayed with fancybox / lightbox
 
 TODO:    
 
 Instagram:
-- add lightbox support for images
 - display name/handle of photographer  
 - implement some sort of caching to prevent loads of traffic to instagr.am servers...
 
--- includes/os_instagramShortcode.php
+++ includes/os_instagramShortcode.php
@@ -5,7 +5,7 @@ Wordpress Short tag to view instagram images
 (c) Odd Egil Hegge Selnes // post@odde.org
 You are free to use this code as you wish - you don't even have to credit me :-)
 
-17. May 2012 - Hooray!
+
 */
 
 add_shortcode('instagram', function($atts) {
@@ -18,7 +18,7 @@ add_shortcode('instagram', function($atts) {
 	);
 	
 	extract($atts);
-	
+		
 	// includes the wordpress class if it's not already available... //
 	if(!class_exists('WP_Http')){
 		include_once( ABSPATH . WPINC. '/class-http.php' );
@@ -33,15 +33,34 @@ add_shortcode('instagram', function($atts) {
 		// displays images, titles and when they were published.... //
 		$xml = new SimpleXMLElement($rss['body']);
  		
-		if($count = count($xml->channel->item)) {
+ 		if($count = count($xml->channel->item)) {
 			// including a stylesheet - go crazy styling this.... :-) //
 			$siteUrl = get_bloginfo('url');
-			echo "<link rel='stylesheet' href='$siteUrl/wp-content/plugins/SocialPlugin/css/instagram.css' />";       
+			echo "<link rel='stylesheet' href='$siteUrl/wp-content/plugins/SocialPlugin/css/instagram.css' />"; 
+
+			// fancybox - lightbox
+			// 1. include jQuery
+			echo "<script type='text/javascript' src='http://ajax.googleapis.com/ajax/libs/jquery/1.7/jquery.min.js'></script>";
+
+			// 2. Mousewheel plugin
+			echo "<script type='text/javascript' src='$siteUrl/wp-content/plugins/SocialPlugin/js/fancybox/lib/jquery.mousewheel-3.0.6.pack.js'></script>";
 			
+			// 3. Fancy boxx      
+			echo "<link rel='stylesheet' href='$siteUrl/wp-content/plugins/SocialPlugin/js/fancybox/source/jquery.fancybox.css?v=2.0.6' type='text/css' media='screen' />";
+			echo "<script type='text/javascript' src='$siteUrl/wp-content/plugins/SocialPlugin/js/fancybox/source/jquery.fancybox.pack.js?v=2.0.6'></script>";
+
+			// 5. Attach fancybox when document is loaded
+			?>
+			<script type="text/javascript">
+				$(document).ready(function() {
+					$(".fancybox").fancybox();
+				});
+			</script>
+			<?php
 			echo "<div id='instagram-feed'>";
 			echo "<ul class='instagram-feed' data-tag='$tag' data-qty='$count'>";
 			foreach($xml->channel->item as $item) {
-				echo "<li><img src='$item->link' alt='$item->title' width='$insta_pic_width' /></li>";
+				echo "<li><a href='$item->link' class='fancybox' rel='group' title='$item->title'><img src='$item->link' alt='$item->title' width='$insta_pic_width' /></a></li>";
 				echo "<li>$item->pubDate</li>";
 				echo "<li>$item->title</li>";
 				echo "<li>&nbsp;</li>";
@@ -51,9 +70,9 @@ add_shortcode('instagram', function($atts) {
 				if($i == $insta_num_pics){
 					break;
 				}// end if 
-			}// end foreach							 
+			}// end foreach			 
 			echo "</ul>";
-			echo "</div>"; 
+			echo "</div>";
 		} else {
 			echo "<!-- No results found for feed $tag -->";
 		}// end if
